24112ec619cfbdcf4cdd977fe1aca91bde2f69c1
c332030 <c332030@c332030.com>
2023-08-28 14:52:59 +0800

    feat: mariadb install

---
+++ script/init.bat
@@ -0,0 +1,16 @@
+@echo off
+
+sc delete mariadb
+
+set app_path=%~dp0
+if exist "%app_path%\data\mysql" (
+    sc create mariadb
+       binpath= "\"%app_path%\app\bin\mysqld.exe\" \"--defaults-file=%app_path%\data\my.ini\" \"mariadb\""
+       start= auto
+) else (
+    cd app\bin
+    mysql_install_db.exe --service=mariadb --password=123456 --datadir=%app_path%\data
+)
+net start mariadb
+
+pause >nul
